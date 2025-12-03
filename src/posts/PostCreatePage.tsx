deb530c53bae4a36043c397bc416b3ec9d4f1a4b
Marco Malaguti <marco@muv.digital>
2025-12-03 09:57:51 +0100

    feat: enhance PostCreatePage with improved YouTube search functionality and UI adjustments

--- src/posts/PostCreatePage.tsx
+++ src/posts/PostCreatePage.tsx
@@ -17,6 +17,8 @@ function PostCreatePage() {
       mediaLinks: mediaLinks,
     };
 
+    console.log("Post payload to send:", postPayload);
+
     const response = await fetch("https://react-vid-app.vercel.app/api/posts", {
       method: "POST",
       headers: {
@@ -79,53 +81,53 @@ function PostCreatePage() {
         <div>
           <input type='submit' className='btn' value='Posten' />
         </div>
-        <hr />
-        <hr />
-        <hr />
-        <hr />
-        <hr />
+      </form>
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
 
-        {/* AB HIER IST DIE YOUTUBE SUCHE */}
+      {/* AB HIER IST DIE YOUTUBE SUCHE */}
+      <div>
+        <h3 className='font-bold text-lg'>Suchen nach Youtube!</h3>
         <div>
-          <h3 className='font-bold text-lg'>Suchen nach Youtube!</h3>
-          <div>
-            <input
-              type='text'
-              placeholder='Type here'
-              id='youtube-search'
-              className='input input-bordered w-full max-w-xs'
-            />
-            <button
+          <input
+            type='text'
+            placeholder='Type here'
+            id='youtube-search'
+            className='input input-bordered w-full max-w-xs'
+          />
+          <button
+            className='btn'
+            onClick={() =>
+              searchYoutube(document.getElementById("youtube-search")?.value)
+            }
+          >
+            Suche
+          </button>
+        </div>
+        {youtubeResults?.map((video) => (
+          <div key={video.id} className='border-b border-gray-300 p-2'>
+            <img src={video.thumbnailUrl} alt={video.title} />
+            <p>{video.title}</p>
+            <a
+              href={`https://www.youtube.com/watch?v=${video.id}`}
+              target='_blank'
               className='btn'
-              onClick={() =>
-                searchYoutube(document.getElementById("youtube-search")?.value)
-              }
+              rel='noreferrer'
             >
-              Suche
+              Link zum Video
+            </a>
+            <button
+              className='btn btn-secondary ml-2'
+              onClick={() => addMediaToPost(video)}
+            >
+              Add to Post
             </button>
           </div>
-          {youtubeResults?.map((video) => (
-            <div key={video.id} className='border-b border-gray-300 p-2'>
-              <img src={video.thumbnailUrl} alt={video.title} />
-              <p>{video.title}</p>
-              <a
-                href={`https://www.youtube.com/watch?v=${video.id}`}
-                target='_blank'
-                className='btn'
-                rel='noreferrer'
-              >
-                Link zum Video
-              </a>
-              <button
-                className='btn btn-secondary ml-2'
-                onClick={() => addMediaToPost(video)}
-              >
-                Add to Post
-              </button>
-            </div>
-          ))}
-        </div>
-      </form>
+        ))}
+      </div>
     </div>
   );
 }
